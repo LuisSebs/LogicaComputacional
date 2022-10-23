1454e9f1e64bbed040447a56556de89b42945d01
LuisSebs <sebastian_luis@ciencias.unam.mx>
2022-10-23 15:19:36 -0500

    modificaciones practica03, se agrego un mensaje de error mas completo y se arreglaron los casos de error de [DFALLA] y [SFALLA]

--- Practica03-ArrietaLuis/practica03.hs
+++ Practica03-ArrietaLuis/practica03.hs
@@ -243,21 +243,23 @@ apSustEq s (t1,t2) = (aplicaT s t1, aplicaT s t2)
 --              Martelli-Montanari
 -- ----------------------------------------------------------------------
 unifmm :: [EqTerm] -> [Sustitucion] -> Sustitucion
-unifmm [] subs = complista subs
+unifmm [] subs = complista subsh[g[g[a]],g[a]]
 unifmm ((V x, t2):eqs) subs = unifmm ((sust (V x , t2) eqs)) (subs ++ [[(V x, t2)]]) -- Aplicar  [SUST]
 unifmm ((t1,V x):eqs) subs = unifmm (swap (t1, V x) ++ eqs) subs -- Aplicar [SWAP]
 unifmm ((T f ts, T g rs):eqs) subs = unifmm (desc (T f ts, T g rs)) subs --Aplicar [DESC]
 
 {-Funcion que aplica la sustitucion-}
 sust :: (Termino,Termino) -> [EqTerm] -> [EqTerm]
-sust (V x, T i ls) [] = []
-sust (V x , T i ls) (e:es) = [(apSustEq [(V x , T i ls)] e)] ++ (sust (V x , T i ls) es)
-sust x y =  error "[SFALLA]"
+sust (V x, t) [] = []
+sust (V x , t) (e:es) 
+    | elem (V x) (variables t) = error ("[SFALLA] ya que " ++ show (V x) ++ "=" ++ show t ++ "no es unificable") 
+    | otherwise = [(apSustEq [(V x , t)] e)] ++ (sust (V x , t) es)
 {-Funcion que aplica la descomposicion-}
 desc :: (Termino,Termino) -> [EqTerm]
 desc (T f [], T g []) = []
-desc (T f (x:xs), T g (y:ys)) = [(x,y)] ++ desc (T f xs, T g ys)
-desc x = error "[DFALLA]"
+desc (T f (x:xs), T g (y:ys)) 
+    | f == g = [(x,y)] ++ desc (T f xs, T g ys)
+    | otherwise = error ("[DFALLA] Los símbolos de función " ++ show f ++ " y " ++ show g ++ "son distintos" )
 {-Funcion que aplica swap-}
 swap :: (Termino,Termino) -> [EqTerm]
 swap (t1, t2) = [(t2,t1)]
---
+++ Practica03-ArrietaLuis/readMe.txt
@@ -0,0 +1,9 @@
+Arrieta Mancera Luis Sebastian
+
+Los únicos problemas que me dio la practica03 fue en la implementacion
+de la funcion unifmm ya que no me quedaba muy claro como implementar 
+las funciones anteriores para esta funcion. Tuve que usar algunas 
+funciones axuiliares como desc, sust y swap en donde si usaba las 
+funciones anteriores. Esta practica a diferencia de las anteriores
+si deja un poco más a conciencia del programador la implementacion
+del código.
